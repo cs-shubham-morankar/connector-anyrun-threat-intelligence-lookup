0ea6e50b60dfc7c5a8930915367cae30e5edb3c6
Semen Shalnev <85073445+semayellow@users.noreply.github.com>
2026-01-14 18:19:21 +0300

    feat(general): implement ti lookup integration

---
+++ anyrun-threat-intelligence-lookup/connector.py
@@ -0,0 +1,26 @@
+from connectors.core.connector import Connector, ConnectorError, get_logger
+
+from .operations import operations, _check_health
+
+logger = get_logger('anyrun-threat-intelligence-lookup')
+
+
+class Any_run(Connector):  # noqa: N801
+    def execute(self, config, operation, params, *args, **kwargs):
+        """ Executes the action """
+        try:
+            logger.info(f'Action name: {operation}')
+            op = operations.get(operation)
+
+            return op(config, params)
+        except Exception as e:
+            logger.exception(f'An exception in execute occurred {e.args}')
+            raise ConnectorError(e) from e
+
+    def check_health(self, config=None, *args, **kwargs):
+        """ Checks connection to ANY.RUN """
+        try:
+            return _check_health(config)
+        except Exception as e:
+            logger.exception(f'An exception in health check occurred {e.args}')
+            raise ConnectorError(e) from e
---
+++ anyrun-threat-intelligence-lookup/constants.py
@@ -0,0 +1,1 @@
+VERSION = 'FortiSOAR:1.0.0'
